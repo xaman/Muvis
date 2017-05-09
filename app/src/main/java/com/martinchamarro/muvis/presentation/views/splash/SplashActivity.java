331392bedbec76e94880ba565b3373e421ceaa79
Martín Chamarro <martin.chamarro@gmail.com>
2017-05-09 22:29:55 +0200

    EDIT -Changes in the splash screen style to show a transparent navigation bar

--- app/src/main/java/com/martinchamarro/muvis/presentation/views/splash/SplashActivity.java
+++ app/src/main/java/com/martinchamarro/muvis/presentation/views/splash/SplashActivity.java
@@ -16,14 +16,18 @@
 
 package com.martinchamarro.muvis.presentation.views.splash;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.Window;
 
 import com.martinchamarro.muvis.R;
 import com.martinchamarro.muvis.presentation.base.BaseActivity;
 
 import javax.inject.Inject;
+
+import static android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
 
 public class SplashActivity extends BaseActivity {
 
@@ -37,7 +41,13 @@ public class SplashActivity extends BaseActivity {
     }
 
     private void configureFullScreen() {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        Window window = getWindow();
+        // Show layout below status bar
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        // Show layout below navigation bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(FLAG_LAYOUT_NO_LIMITS, FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 
     @Override protected int getLayoutResource() {
