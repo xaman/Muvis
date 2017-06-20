d332bc2989e0708943fa026e65f42e72f9e3aa8e
Martín Chamarro <martin.chamarro@gmail.com>
2017-06-21 00:06:44 +0200

    EDIT -Refactoring of the mapper to use 'let' instead of return null

--- app/src/main/kotlin/com/martinchamarro/muvis/data/mapper/DetailEntityMapper.kt
+++ app/src/main/kotlin/com/martinchamarro/muvis/data/mapper/DetailEntityMapper.kt
@@ -25,16 +25,17 @@ class DetailEntityMapper @Inject constructor(
         private val countryMapper: CountryEntityMapper) {
 
     operator fun invoke(entity: DetailEntity?): Detail? {
-        return if (entity != null) Detail(
-            budget = entity.budget,
-            homepage = entity.homepage,
-            imdbId = entity.imdbId,
-            originalLanguage = entity.originalLanguage,
-            originalTitle = entity.originalTitle,
-            overview = entity.overview,
-            runtime = entity.runtime,
-            genres = entity.genres.map { genreMapper(it) }.toList(),
-            countries = entity.countries.map { countryMapper(it) }.toList()
-        ) else null
+        return entity?.let {
+            Detail(
+                    budget = entity.budget,
+                    homepage = entity.homepage,
+                    imdbId = entity.imdbId,
+                    originalLanguage = entity.originalLanguage,
+                    originalTitle = entity.originalTitle,
+                    overview = entity.overview,
+                    runtime = entity.runtime,
+                    genres = entity.genres.map { genreMapper(it) }.toList(),
+                    countries = entity.countries.map { countryMapper(it) }.toList())
+        }
     }
 }
